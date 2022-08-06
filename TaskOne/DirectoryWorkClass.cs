f8846848da7a56ae3e6852b0a4035d99d1e28cc8
Надежда Саганенко <nadinvgs@gmail.com>
2022-08-06 22:30:33 +0300

    Added read and write methods

--- TaskOne/DirectoryWorkClass.cs
+++ TaskOne/DirectoryWorkClass.cs
@@ -1,8 +1,78 @@
-﻿namespace TaskOne
+﻿using System.Text;
+
+namespace TaskOne
 {
     public class DirectoryWorkClass
     {
         public string SourceFolder { get; set; }
         public string ResultFolder { get; set; }
+
+        public string[] SearchFiles;
+
+        public IEnumerable<List<string>> ReadFile(string path)
+        {
+            List<string> Lines = new List<string>();
+
+            using (StreamReader stream = new StreamReader(path))
+            {
+                int count = 0;
+                while (stream.EndOfStream != true)
+                {
+                    string line = stream.ReadLine();
+                    if (line != null && line.Contains("first_name:"))
+                        continue;
+                    else
+                    {
+                        Lines.Add(line);
+                        count++;
+                    }
+                    if (count == 100)
+                    {
+                        count = 0;
+                        yield return Lines;
+                        Lines.Clear();
+                    }
+                }
+                if (count > 0 && count <= 100)
+                {
+                    yield return Lines;
+                }
+            }
+        }
+
+        public string CreateResultFileName ()
+        {
+            DateTime dateTime = DateTime.Now;
+            string nameFolder = $"{dateTime.Month}-{dateTime.Day}-{dateTime.Year}";
+            string path = this.ResultFolder + "\\" + nameFolder;
+
+            var directory = new DirectoryInfo(path);
+            if (!directory.Exists)
+            {
+                directory.Create();
+            }
+            int countExistFiles = Directory.GetFiles(path).Length;
+            path = $"{path}\\output{(++countExistFiles).ToString()}.json";
+
+            return path;
+        }
+
+        public void WriteDataToFile(string pathOldFile, string pathNewFile, string jsonString)
+        {         
+            try
+            {
+                using (FileStream fs = File.Create(pathNewFile))
+                {
+                    byte[] info = new UTF8Encoding(true).GetBytes(jsonString);
+                    fs.Write(info, 0, info.Length);
+                }              
+                FileInfo fileInfo = new FileInfo(pathOldFile);
+                fileInfo.Delete();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
     }
 }
--- TaskOne/Program.cs
+++ TaskOne/Program.cs
@@ -1,8 +1,77 @@
 ﻿using Microsoft.Extensions.Configuration;
+using System.Text.Json;
+using TaskOne;
 
 var configuration = new ConfigurationBuilder()
      .SetBasePath(Directory.GetCurrentDirectory())
      .AddJsonFile($"appsettings.json").Build();
 
+DirectoryWorkClass directoryWorkClass = new DirectoryWorkClass()
+{
+    SourceFolder = configuration.GetSection("path:folderA").Value,
+    ResultFolder = configuration.GetSection("path:folderB").Value,
+    SearchFiles = new string[2] { "*.csv", "*.txt" }
+};
 
+while (true)
+{
+    foreach (string filePath in directoryWorkClass.SearchFiles
+                            .AsParallel()
+                            .SelectMany(SearchPattern => Directory.EnumerateFiles(directoryWorkClass.SourceFolder, SearchPattern)))
+    {
+        List<PaymentTransaction> listObject = new List<PaymentTransaction>();
+        try
+        {
+            foreach (List<string> LinesPart in directoryWorkClass.ReadFile(filePath))
+            {
+                LinesPart.ForEach(x =>
+                {
+                    x = x.Replace("“", "").Replace("\"", "").Replace("”", "");
+                    List<string> mas = x.Split(new char[] { ',' }).ToList<string>();
 
+                    Payer payer = new Payer() { Name = mas[0].Trim(), Account_number = Convert.ToInt64(mas[7].Trim()), date = mas[6].Trim(), Payment = Convert.ToDecimal(mas[5].Trim().Replace(".", ",")) };
+
+                    var city = listObject.FirstOrDefault(x => x.City == mas[2].Trim());
+                    if (city != null)
+                    {
+                        var service = city.Services.FirstOrDefault(x => x.Name == mas[8].Trim());
+                        if (service != null)
+                        {
+                            service.Payers.Add(payer);
+                            service.Total += payer.Payment;
+                            service.Total = service.Payers.Sum(x => x.Payment);
+                        }
+                        else
+                        {
+                            Service serviceNew = new Service() { Name = mas[8].Trim(), Total = payer.Payment, Payers = new List<Payer>() };
+                            serviceNew.Payers.Add(payer);
+                            city.Services.Add(serviceNew);
+                        }
+                        city.Total = city.Services.Sum(x => x.Total);
+                    }
+                    else
+                    {
+                        PaymentTransaction paymentTransaction = new PaymentTransaction() { City = mas[2].Trim(), Total = payer.Payment, Services = new List<Service>() };
+
+                        Service service = new Service() { Name = mas[8].Trim(), Payers = new List<Payer>() };
+                        service.Payers.Add(payer);
+                        service.Total = payer.Payment;
+
+                        paymentTransaction.Services.Add(service);
+                        listObject.Add(paymentTransaction);
+                    }
+                });
+            }
+
+            string jsonString = JsonSerializer.Serialize(listObject);
+            string pathNewFile = directoryWorkClass.CreateResultFileName();
+
+            directoryWorkClass.WriteDataToFile(filePath, pathNewFile, jsonString);
+        }
+        catch
+        {
+            continue;
+        }
+
+    }
+}
